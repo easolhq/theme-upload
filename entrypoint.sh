387641ec07366aba3f20877991045a2b74f2b27e
Kyle Byrne <kyletbyrne96@gmail.com>
2021-09-08 14:13:20 +0100

    Add entrypoint for aws s3 syncing
    We use the s3 sync action from the aws cli to sync the repo with the
    bucket in S3. The --delete option means that we clean up any files which
    are removed from the theme repo in S3.
    The three required keys are what the user of the action will set on the
    theme repo as supplied by an Easol member of staff.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,34 @@
 # Exit on first error
 set -e
 
-echo "Hello world"
+# Assert that the required keys have been set
+if [ -z ${EASOL_KEY} ]; then
+  echo "EASOL_KEY not defined, unable to authorize"
+  exit 1
+fi
+
+if [ -z ${EASOL_SECRET} ]; then
+  echo "EASOL_SECRET not defined, unable to authorize"
+  exit 1
+fi
+
+if [ -z ${THEME_KEY} ]; then
+  echo "THEME_KEY not defined, required to add theme"
+  exit 1
+fi
+
+# Setup aws credentials for us to authorize
+mkdir ~/.aws
+touch ~/.aws/credentials
+
+cat << EOF > ~/.aws/credentials
+[default]
+aws_access_key_id=${EASOL_KEY}
+aws_secret_access_key=${EASOL_SECRET}
+EOF
+
+# Sync the current repo with the theme catalogue on S3
+aws s3 sync . s3://easol-themes/${THEME_KEY} --delete --no-progress --region eu-west-1
+
+# Remove aws creds
+rm -rf ~/.aws
